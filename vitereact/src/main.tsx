1a7308a869706a7d3db14f371445e3411c4abcae
VersaceXcodes <95313545+VersaceXcodes@users.noreply.github.com>
2025-06-26 12:54:29 +0100

    Deploy attempt 2 - what-to-watch

--- vitereact/src/main.tsx
+++ vitereact/src/main.tsx
@@ -1,12 +1,12 @@
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
-import store from "@/store/main";
+import { useAppStore } from "@/store/main";
 import AppWrapper from "./AppWrapper.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-	<Provider store={store}>
+	<Provider store={useAppStore()}>
 		<BrowserRouter>
 			<AppWrapper />
 		</BrowserRouter>
